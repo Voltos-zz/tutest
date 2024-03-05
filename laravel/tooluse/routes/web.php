0155108f992ceef26a8f81e36faf127a4ed9d436
voltos <voltos@inbox.ru>
2024-03-05 13:34:57 +0000

    add htaccess file

--- laravel/tooluse/routes/web.php
+++ laravel/tooluse/routes/web.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('test', function () {
     return view('welcome');
 });
 
-Route::get('/control', function () {
-    return view('managerLogin');
-});
